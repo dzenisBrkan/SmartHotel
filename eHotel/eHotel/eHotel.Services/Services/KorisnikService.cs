2423b6ae1a95efcce944e7883bb75a3525981d32
Tarik <tarikkke@hotmail.com>
2026-06-23 18:30:08 +0200

    Adding Services for Clients, Reservations and Payment

--- eHotel/eHotel/eHotel.Services/Services/KorisnikService.cs
+++ eHotel/eHotel/eHotel.Services/Services/KorisnikService.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using eHotel.Database;
+using eHotel.Dto.Korisnici;
 using eHotel.eHotel.Services.Interface;
+using System.Data.Entity;
 
 namespace eHotel.eHotel.Services.Services;
 
@@ -15,8 +17,119 @@ public class KorisnikService : IKorisnikService
         _mapper = mapper;
     }
 
-    public List<Model.Korisnici> Get()
+    //public List<Model.Korisnici> Get()
+    //{
+    //    return _context.Korisnicis.ToList().Select(x => _mapper.Map<Model.Korisnici>(x)).ToList();
+    //}
+
+
+    public async Task<List<Korisnici>> GetAsync(string? imePrezime = null)
     {
-        return _context.Korisnicis.ToList().Select(x => _mapper.Map<Model.Korisnici>(x)).ToList();
+        var query = _context.Korisnicis.AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(imePrezime))
+        {
+            query = query.Where(x =>
+                (x.Ime + " " + x.Prezime).Contains(imePrezime));
+        }
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<Korisnici?> GetByIdAsync(int id)
+    {
+        return await _context.Korisnicis.FindAsync(id);
+    }
+
+    public async Task<Korisnici> InsertAsync(KorisniciInsertRequest request)
+    {
+        var entity = new Korisnici
+        {
+            Ime = request.Ime,
+            Prezime = request.Prezime,
+            Email = request.Email,
+            KorisnickoIme = request.KorisnickoIme,
+            PasswordHash = "Test"
+        };
+
+        _context.Korisnicis.Add(entity);
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<Korisnici?> UpdateAsync(int id,
+        KorisniciUpdateRequest request)
+    {
+        var entity = await _context.Korisnicis.FindAsync(id);
+
+        if (entity == null)
+            return null;
+
+        entity.Ime = request.Ime;
+        entity.Prezime = request.Prezime;
+        entity.Email = request.Email;
+
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var entity = await _context.Korisnicis.FindAsync(id);
+
+        if (entity == null)
+            return false;
+
+        _context.Korisnicis.Remove(entity);
+
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
+
+    //public async Task<Korisnici?> LoginAsync(string korisnickoIme,
+    //    string lozinka)
+    //{
+    //    return await _context.Korisnicis
+    //        .FirstOrDefaultAsync(x =>
+    //            x.KorisnickoIme == korisnickoIme &&
+    //            x.LozinkaHash == lozinka);
+    //}
+
+    public async Task<Korisnici?> GetProfilAsync(int korisnikId)
+    {
+        return await _context.Korisnicis
+            .FirstOrDefaultAsync(x => x.KorisnikId == korisnikId);
+    }
+
+    public async Task<Korisnici?> UpdateProfilAsync(
+        int korisnikId,
+        KorisnikProfilUpdateRequest request)
+    {
+        var korisnik = await _context.Korisnicis
+            .FindAsync(korisnikId);
+
+        if (korisnik == null)
+            return null;
+
+        korisnik.Ime = request.Ime;
+        korisnik.Prezime = request.Prezime;
+        korisnik.Email = request.Email;
+        korisnik.Telefon = request.Telefon;
+
+        await _context.SaveChangesAsync();
+
+        return korisnik;
+    }
+
+    public async Task<List<Korisnici>> GetZaposleniciAsync()
+    {
+        return await _context.KorisniciUloges
+            .Where(x => x.Uloga.Naziv == "Zaposlenik")
+            .Select(x => x.Korisnik)
+            .ToListAsync();
     }
 }
+
